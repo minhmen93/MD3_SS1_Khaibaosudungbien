27d1f39e05bc4c3890afc868a410c88053db66ad
unknown <minhmen93@gmail.com>
2023-03-07 11:02:37 +0700

    ss1 khai bao su dung bien

---
+++ src/Khaibaosudungbien.java
@@ -0,0 +1,15 @@
+public class Khaibaosudungbien {
+    public static void main(String[] args) {
+
+        int i = 10; // khai báo biến i là kiểu int có giá trị là 10
+        float f = 20.5f; // khai báo biến f là kiểu float có giá trị là 20.5
+        boolean b = true; // khai báo biến b là kiểu boolean có giá trị là true
+        char c = 'a'; // khai asoo biến c là kiểu char có giá trị là kí tự a
+        String s = "Hà Nội"; // khai báo biến s là kiểu string có giá trị là chuỗi hà Nội
+        System.out.println("i = " + i);
+        System.out.println("f = " +f);
+        System.out.println("c = "+c);
+        System.out.println("b = "+b);
+        System.out.println("s = "+s);
+    }
+}
